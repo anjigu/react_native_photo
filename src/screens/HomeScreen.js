a5134b9a80f21c532e19f1722e099106d87f9402
안지수 <anjisu@anjisuui-MacBookPro.local>
2023-02-27 14:16:41 +0900

    feat: 탭 내비게이터 아이콘 지정

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,24 +1,22 @@
-import { StyleSheet, Text, View } from "react-native"
-import { WHITE } from "../colors";
+import { StyleSheet, Text, View } from 'react-native';
+import { WHITE } from '../colors';
 
 const HomeScreen = () => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>HomeScreen</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>HomeScreen</Text>
+    </View>
+  );
 };
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1, 
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: WHITE,
-    },
-    title: {
-        fontSize: 30,
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: WHITE,
+  },
+  title: {
+    fontSize: 30,
+  },
 });
-
 export default HomeScreen;
